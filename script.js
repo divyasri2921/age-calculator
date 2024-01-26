f1e99f779f976b11eca42b96613f952a8f2258b1
divyasri2921 <149350977+divyasri2921@users.noreply.github.com>
2024-01-26 12:50:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+// const dateInput=document.querySelector("input");
+// const pElement=document.querySelector("p");
+
+// dateInput.addEventListener("change",() => {
+//     const birthDate = new Date(dateInput.value);
+//     const currentDate = new Date();
+//     const newDate =new Date(currentDate - birthDate);
+
+//     const Years = newDate.getfullYear()-1985;
+//     const Months = newDate.getMonth();
+//     const days = newDate.getDate() - 1;
+
+//     pElement.innerText = 'Hello! welcome youre'  
+//     + Years +"Years" + Month + 'Month' + Days+ 'Days Old.';
+// })
+//     const Now = new Date();
+// console.log(Now);
+// console.log(Now.getMonth());
+// console.log(Now.getFullYear())
+
+// const birthDate =new Date(2024,0,26)
+// console.log(birthDate);
+// days = birthDate- Now;
+// console.log(days);
+
+
+// const dateInput=document.querySelector("Input");
+// const currentDate= new Date ();
+// const birthDate=new Date(1999,9,29);
+// const  Days=currentDate.getDays();
+// const newDate= currentDate-birthDate;
+// newDate =parseInt(Math.floor(Days/86400000))
+// console.log(newDate)
+
+// const pElement=document.querySelector("p");
+// getBirthday.addEventListener("Input",()=>{
+    
+//     Days = getBirthday-Now;
+
+//     pElement.innerText = 'Hello! welcome youre'  
+//     + Years +"Years" + Month + 'Month' + Days+ 'Days Old.';
+// })
+
+let userInput =document.getElementById("date");
+userInput.max = new Date().toISOString().split("T")[0];
+ 
+function calculateAge(){
+    let birthDate = new Date(userInput.value);
+
+    let d1 = birthDate.getDate();
+    let m1 =birthDate.getMonth() + 1;
+    let y1 =birthDate.getFullYear();
+
+    let today = new Date();
+
+    let d2 = today.getDate();
+    let m2 = today.getMonth() +1 ;
+    let y2 = today.getFullYear();
+
+}
